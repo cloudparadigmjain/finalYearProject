e1c4383006fd3b4c204cf3899c84eb02a770c630
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2020-06-18 22:13:38 +0530

    trial

--- public/autocomplete.js
+++
@@ -1,9 +0,0 @@
-
-function initialize() {
-var input = document.getElementById('searchTextField');
-new google.maps.places.Autocomplete(input);
-var input1 = document.getElementById('searchTextField2');
-new google.maps.places.Autocomplete(input1);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
